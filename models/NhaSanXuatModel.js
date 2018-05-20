7ccd210824ff81800f6f9caf1851c8a62e00e735
BARCELONA\user <hoangphuct97@gmail.com>
2018-05-20 20:02:51 +0700

    update layout admin, liet ke va cap nhat du lieu laptop, nha san xuat. Them chuc nang tim kiem

--- models/NhaSanXuatModel.js
+++ models/NhaSanXuatModel.js
@@ -1,10 +1,21 @@
 var mongoose = require("mongoose");
 
+var mongoDB = "mongodb://tolu5455:intheend54@ds121730.mlab.com:21730/computer";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error'));
+
 var Schema = mongoose.Schema;
 var nhasanxuatSchema = new Schema({
   ten: { type: String, required: [true, "Tên trống"], max: 100 },
   hinh:{ type: String, required:[true, "Đường dẫn trống"]},
   trungtambaohanh: {type: String, required: [true, "Trung tâm bảo hành trống"], max: 100}
 });
+
+nhasanxuatSchema.virtual('url').get(function(){
+  return '/catalog/nhasanxuat/' + this._id;
+})
 
 module.exports = mongoose.model("Nhasanxuat", nhasanxuatSchema);
